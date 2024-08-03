2fc47854f8696356e57b67dc4eb06677cb2e6947
Tridev9 <triddev123@gmail.com>
2024-08-03 17:05:06 +0530

    3/8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 import React from 'react'
 
 const App = () => {
   return (
-    <h1>
-      Welcome to Smile Dream Cakes
-    </h1>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
